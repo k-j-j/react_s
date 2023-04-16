79f3b011e4626eb6b210260cb8e9f1734d2d7f06
k-j-j <eoq276@naver.com>
2023-04-16 12:40:43 +0900

    sc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import './reset.css';
 import './App.css';
+import $ from 'jquery'; 
 
 function App() {
+
+  $(function(){
+    const menu = $('#top_menu > ul > li');
+    const contents = $('#contents > div');
+    
+    menu.click(function(event){
+        event.preventDefault();
+        
+        const tg = $(this);
+        const i = tg.index();
+        
+        const section = contents.eq(i);
+        const tt = section.offset().top;
+        
+        $('html, body').stop().animate({scrollTop:tt});
+     });
+
+     $(window).scroll(function(){
+      const sct = $(window).scrollTop();
+      contents.each(function(){
+          const tg1 = $(this);
+          const i = tg1.index();
+           if(tg1.offset().top <= sct){
+               menu.removeClass('on');
+               menu.eq(i).addClass('on');
+           };
+           
+       });
+
+   });
+
+
+
+
+});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="wrap">
+			<div id="top_menu">
+					
+				<div id="logo">
+					<a href="#home"><img src="img/logoPaper.png" alt="Paper" /></a>
+				</div>
+				<ul>
+					<li className="on"><a href="#paper">paper</a></li>
+					<li><a href="#works">works</a></li>
+					<li><a href="#email">email</a></li>
+				</ul>
+					
+			</div>
+			<div id="contents">
+				<div id="paper"><img src="img/paper.jpg" alt="paper" /></div>
+				<div id="works"><img src="img/work.jpg" alt="works" /></div>
+				<div id="email"><img src="img/email.jpg" alt="email" /></div>
+			</div>
+		</div>
   );
 }
 
